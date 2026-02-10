48ead3152b50f11dba5d8cba61a649444f5b1618
Max lambert <max.lambert429@gmail.com>
2026-02-10 07:42:03 -0700

    Creation of baseplate

---
+++ classification-webapp/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PetScan | AI Cat/Dog Classifier",
+  description: "A web application built in Next.js that is attached to a FastAPI Python backend. This website allows you to upload photos and feed them to a ML Convolutional Neural Network; Which then determines whether or not is it a dog, cat, or neither, and then returns that result to the user alongside a confidence metric.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ classification-webapp/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import styles from "@styles/home.module.css"
+export default function Home() {
+  return (
+    <div className={styles.homeWrapper}>
+        
+    </div>
+  );
+}
